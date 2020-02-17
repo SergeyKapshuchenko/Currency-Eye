cd5de0765eb7268f9c6663f0d9fdd0ab2d27bd28
sergeykapshuchenko <Serg3y1999@gmail.com>
2020-02-17 10:06:46 +0200

    requirements.txt

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/sergeykapshuchenko/Desktop/CurrencyEye/venv"
+set -gx VIRTUAL_ENV "/Users/sergeykapshuchenko/Desktop/Python/CurrencyEye/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
